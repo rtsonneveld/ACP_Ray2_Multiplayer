b2af1d1cd0f87c4a851e77d1f75eba6abcc391c3
Robin <robin@redbirdstudios.net>
2026-07-20 21:12:01 +0200

    Initialize repo

---
+++ dllmain.c
@@ -0,0 +1,21 @@
+#include "framework.h"
+
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD dwReason, LPVOID lpReserved )
+{
+	switch ( dwReason )
+	{
+		case DLL_PROCESS_ATTACH: /* create function hooks here */
+			/* FHK_fn_lCreateHook((void**)&GAM_fn_vEngine, (void*)MOD_fn_vEngine); */
+			break;
+
+		case DLL_PROCESS_DETACH: /* destroy function hooks here*/
+			/* FHK_fn_lDestroyHook((void**)&GAM_fn_vEngine, (void*)MOD_fn_vEngine); */
+			break;
+
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+			break;
+	}
+	return TRUE;
+}
